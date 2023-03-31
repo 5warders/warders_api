f71cd3658be65db71be8e43aff10d951bdfbe3b3
jeonjonghyeok <tongher1685@gmail.com>
2023-03-31 23:31:57 +0900

    [feature/profile] 프로필 기능

--- src/main/java/com/warders/api/common/config/WebConfig.java
+++ src/main/java/com/warders/api/common/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.warders.api.common.argumentresolver.UserIdArgumentResolver;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new UserIdArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000");
     }
 }
